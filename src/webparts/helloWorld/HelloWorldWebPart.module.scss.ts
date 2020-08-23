b27eb0eb9620baf13de3193bf9c3e865489cd778
Satendra Kumar Prasad <60088760+satendra97@users.noreply.github.com>
2020-08-23 18:37:58 +0530

    Add files via upload

---
+++ src/webparts/helloWorld/HelloWorldWebPart.module.scss.ts
@@ -0,0 +1,19 @@
+/* tslint:disable */
+require("./HelloWorldWebPart.module.css");
+const styles = {
+  helloWorld: 'helloWorld_c6c07fe7',
+  container: 'container_c6c07fe7',
+  row: 'row_c6c07fe7',
+  column: 'column_c6c07fe7',
+  'ms-Grid': 'ms-Grid_c6c07fe7',
+  title: 'title_c6c07fe7',
+  subTitle: 'subTitle_c6c07fe7',
+  description: 'description_c6c07fe7',
+  list: 'list_c6c07fe7',
+  listItem: 'listItem_c6c07fe7',
+  button: 'button_c6c07fe7',
+  label: 'label_c6c07fe7'
+};
+
+export default styles;
+/* tslint:enable */
